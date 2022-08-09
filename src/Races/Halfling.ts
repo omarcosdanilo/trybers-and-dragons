66d491791846392c3ef5bba53e0a6e730ddeb1fc
Marcos Danilo <marcosdanilo94@gmail.com>
2022-08-09 19:55:21 -0300

    Cria a classe Halfling

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,26 @@
+import Race from './Race';
+
+class Halfling extends Race {
+  private static _counter = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._name = name;
+    this._dexterity = dexterity;
+    this._maxLifePoints = 60;
+
+    Halfling._counter += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Halfling._counter;
+  }
+
+  get maxLifePoints() {
+    return this._maxLifePoints;
+  }
+}
+
+export default Halfling;
